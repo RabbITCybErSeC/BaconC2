c426a4b89085474d409d7e80109c0f1dd2633e8f
RabbITCybErSeC <janpaul.konijn@adyen.com>
2025-11-05 18:55:46 +0100

    file rename

--- pkg/plugins/interfaces.go
+++ pkg/plugins/interfaces.go
@@ -7,7 +7,6 @@ import (
 	"github.com/RabbITCybErSeC/BaconC2/pkg/models"
 )
 
-// PluginCapability represents what a plugin can do
 type PluginCapability string
 
 const (
@@ -19,7 +18,6 @@ const (
 	CapabilityCustom         PluginCapability = "custom"
 )
 
-// PluginStatus represents the current state of a plugin
 type PluginStatus string
 
 const (
@@ -31,7 +29,6 @@ const (
 	PluginStatusUnloading PluginStatus = "unloading"
 )
 
-// PluginMetadata contains information about a plugin
 type PluginMetadata struct {
 	Name         string             `json:"name"`
 	Version      string             `json:"version"`
@@ -45,48 +42,39 @@ type PluginMetadata struct {
 	Hash         string             `json:"hash,omitempty"`
 }
 
-// PluginContext provides runtime context to plugins
 type PluginContext struct {
 	AgentState   command_handler.IAgentState
 	Registry     *command_handler.CommandHandlerRegistry
 	Config       map[string]interface{}
 }
 
-// IPlugin is the main interface that all plugins must implement
-// This is the interface that compiled .so files must export
 type IPlugin interface {
-	// GetMetadata returns plugin metadata
 	GetMetadata() PluginMetadata
-
-	// Initialize is called when the plugin is loaded
 	Initialize(ctx *PluginContext) error
-
-	// Execute handles a command and returns a result
 	Execute(cmd models.Command) models.CommandResult
-
-	// Cleanup is called when the plugin is unloaded
 	Cleanup() error
-
-	// GetStatus returns the current status of the plugin
 	GetStatus() PluginStatus
 }
 
-// IStatefulPlugin extends IPlugin with stateful command handling
 type IStatefulPlugin interface {
 	IPlugin
-
-	// ExecuteStateful handles a command with access to agent state
 	ExecuteStateful(ctx *command_handler.CommandContext) models.CommandResult
 }
 
-// PluginFactory is the function signature that plugin .so files must export
-// The symbol name must be "NewPlugin"
 type PluginFactory func() IPlugin
 
-// PluginInfo contains information about a plugin file
 type PluginInfo struct {
 	Metadata PluginMetadata `json:"metadata"`
 	FilePath string         `json:"file_path"`
 	FileSize int64          `json:"file_size"`
 	Hash     string         `json:"hash"`
+}
+
+// IPluginLoader defines the interface for plugin loaders
+type IPluginLoader interface {
+	GetInstance(identifier string) (IPlugin, bool)
+	ListLoaded() []string
+	Unload(identifier string) error
+	LoadFromFile(filePath string) (IPlugin, error)
+	LoadFromBytes(name string, data []byte) (IPlugin, error)
 }
